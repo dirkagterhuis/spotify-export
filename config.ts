dc1f7efb8c7823d141c09d825033618f6b70ae65
Dirk <dirk.agterhuis@new10.com>
2022-03-24 23:02:07 +0100

    chore: updated to ssl

--- config.ts
+++ config.ts
@@ -13,7 +13,6 @@ if (process.env.NODE_ENV === undefined || process.env.NODE_ENV === 'dev') {
 
 export const config = {
     env: process.env.NODE_ENV || 'dev',
-    //TODO: change to https when secure.
     baseUrl: process.env.NODE_ENV ? 'http://spotifyexport.com' : 'http://localhost:8000',
     spotifyClientId: process.env.SPOTIFY_APP_CLIENT_ID || localConfig.localSpotifyAppClientId,
     spotifyClientSecret:
