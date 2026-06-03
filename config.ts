c64cb9307c7b230caa36057c3ce30a4ff5fdd5e3
Dirk <dirk.agterhuis@new10.com>
2026-06-03 22:49:45 +0200

    chore: typing: added noImplicitAny to config

--- config.ts
+++ config.ts
@@ -1,11 +1,16 @@
 import fs from 'fs'
 import yaml from 'js-yaml'
 
+interface LocalConfig {
+    localSpotifyAppClientId: string
+    localSpotifyAppClientSecret: string
+}
+
 // Only set localConfig in dev
-let localConfig
+let localConfig: LocalConfig | undefined
 if (process.env.NODE_ENV === undefined || process.env.NODE_ENV === 'dev') {
     try {
-        localConfig = yaml.load(fs.readFileSync('./local.yml', 'utf8'))
+        localConfig = yaml.load(fs.readFileSync('./local.yml', 'utf8')) as LocalConfig
     } catch (e) {
         throw new Error(`In dev environment, localConfig isn't configured correctly: ${e.message}`)
     }
@@ -13,8 +18,10 @@ if (process.env.NODE_ENV === undefined || process.env.NODE_ENV === 'dev') {
 
 export const config = {
     env: process.env.NODE_ENV || 'dev',
-    baseUrl: process.env.NODE_ENV ? 'https://spotifyexport.com' : 'https://dev.spotifyexport.com:8000',
-    spotifyClientId: process.env.SPOTIFY_APP_CLIENT_ID || localConfig.localSpotifyAppClientId,
+    baseUrl: process.env.NODE_ENV
+        ? 'https://spotifyexport.com'
+        : 'https://dev.spotifyexport.com:8000',
+    spotifyClientId: process.env.SPOTIFY_APP_CLIENT_ID || localConfig?.localSpotifyAppClientId,
     spotifyClientSecret:
-        process.env.SPOTIFY_APP_CLIENT_SECRET || localConfig.localSpotifyAppClientSecret,
+        process.env.SPOTIFY_APP_CLIENT_SECRET || localConfig?.localSpotifyAppClientSecret,
 }
